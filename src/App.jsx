1a96a26f6477a1ee64fa06154ce80d88bbf47ea2
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2023-11-30 19:43:01 +0000

    COMPONENTS:  - AppBarComponent created
    LAYOUT:
     - index file initiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { useEffect } from 'react'
+import { RouterProvider } from 'react-router-dom'
 
 import { getAllUsers } from './services/userService'
+
+import config from './router'
 
 import './App.css'
 
 function App() {
   
-  useEffect(() => {
-    const getData = async () => {
-      const result = await getAllUsers()
-      console.log(result)
-    }
-    getData()
-  }, [])
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const result = await getAllUsers()
+  //     console.log(result)
+  //   }
+  //   getData()
+  // }, [])
 
   return (
     <>
-      
+      <RouterProvider router={ config } />
+
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import AppBarComponent from '../components/AppBarComponent/AppBarComponent'
+
+const Home = () => {
+  return (
+    <>
+      <AppBarComponent />
+    </>
+  )
+}
+
+export default Home
